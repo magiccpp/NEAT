50ae3bf29ebc217fdccdb695fb26483ffc072989
ken <smartestrobotdai>
2026-02-28 14:24:52 +0800

    updated run daily script

--- run_daily.sh
+++ run_daily.sh
@@ -3,6 +3,7 @@
 source /home/ken/anaconda3/etc/profile.d/conda.sh
 # Activate the desired conda environment
 conda activate stock
+cd /home/ken/git/NEAT
 # Run the batch processing script
 python3 claude-2-mul-thread-gpt5-vol-selection-output-ckpoint.py --max-population=2000 \
 --log-file=data_vol-cluster-0-startdate-1962-01-03.log  \
@@ -11,3 +12,5 @@ python3 claude-2-mul-thread-gpt5-vol-selection-output-ckpoint.py --max-populatio
 --json-output-dir=./json-output \
 --checkpoint-dir=checkpoints-startdate-1962-01-03 \
 --reload --long-only
+
+cp -rf ./json-output/* ../orch/models_output_json/NEAT_62/
